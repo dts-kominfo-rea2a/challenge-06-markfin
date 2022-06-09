60e6c247df991ededb2133a06647ee59ce956af6
markfin <marfinzamarudin@gmail.com>
2022-06-09 21:58:52 +0900

    menambahkan data di fungsiCustom

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,7 @@
 // TODO: import module bila dibutuhkan di sini
 
+const fs = require("fs");
+
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -7,23 +9,46 @@ let file3 = "./data3.json";
 
 // ! JANGAN DIMODIFIKASI
 let modifyFile1 = (val) => {
-  file1 = val;
+    file1 = val;
 };
 let modifyFile2 = (val) => {
-  file2 = val;
+    file2 = val;
 };
 let modifyFile3 = (val) => {
-  file3 = val;
+    file3 = val;
 };
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+    fs.readFile(file1, "utf-8", (err, content1) => {
+        const data1 = JSON.parse(content1);
+        const message1 = getKataAkhir(data1.message);
 
+        fs.readFile(file2, "utf-8", (err, content2) => {
+            const data2 = JSON.parse(content2);
+            const message2 = getKataAkhir(data2[0].message);
+
+            fs.readFile(file3, "utf-8", (err, content3) => {
+                const data3 = JSON.parse(content3);
+                const message3 = getKataAkhir(data3[0].data.message);
+
+                fnCallback(err, [message1, message2, message3]);
+            });
+        });
+    });
+};
+
+
+const getKataAkhir = (sentence) => {
+    const arrSentence = sentence.split(" ").reverse();
+
+    return arrSentence[0];
+};
 // ! JANGAN DIMODIFIKASI
 module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
+    modifyFile1,
+    modifyFile2,
+    modifyFile3,
+    bacaData,
 };
